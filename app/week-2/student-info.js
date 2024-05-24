27ad524a0d2f0d94e22a05a47fd9c935378b9e3c
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-05-24 10:03:05 -0600

    Asignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>Your name: Khushpreet Singh Gill</p>
+      <p>GitHub Repository: <a href="https://github.com/Khush5882/cprg306-assignments">https://github.com/Khush5882/cprg306-assignments</a></p>
+    </div>
+  );
+};
+
+export default StudentInfo;

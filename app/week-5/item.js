27303654d13fef951a8761ad809edc00be4dd0ec
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-06-25 18:40:53 -0600

    week 5

---
+++ app/week-5/item.js
@@ -0,0 +1,15 @@
+
+export function Item({name, quantity ,category}) {
+
+
+    return (
+        // name,quantity,category
+        
+       <div className=" m-4 p-4 text-white bg-slate-900 rounded w-1/4 h-1/3 ">
+        <h2 className="font-bold text-xl">{name}</h2> 
+        <p className="text-sm">Buy {quantity} in {category}</p>
+       </div>
+        
+      
+    );
+  }
